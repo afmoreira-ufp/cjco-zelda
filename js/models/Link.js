f3e1130c7b24b7eae9cbfc65a9f2fdf3915a944e
Alessandro <afmoreira@ufp.edu.pt>
2020-03-09 15:19:36 +0000

    refactorization done

--- js/models/Link.js
+++ js/models/Link.js
@@ -5,54 +5,176 @@ export default class Link extends Phaser.Physics.Arcade.Sprite {
     scene.add.existing(this);
     scene.physics.add.existing(this);
     scene.physics.world.enable(this);
-    this.setFrame(14);
-    this.setScale(2.5);
-    this.direction = "down";
-    this.setSize(24, 24);
-    this.setOffset(-1, 0);
+    //this.setFrame(14);
+    this.setScale(2.0, 2.0);
+    this.direction = "idle";
+    this.setSize(20, 20);
+    this.setOffset(1, 1);
     // create a group for all the player's hitboxes
     //hitboxes = game.add.group();
-    this.hitboxes = this.scene.add.group();
-    //this.tint=0xff00ff;
+    this.hitboxes = this.scene.physics.add.group({
+      maxSize: 1
+    });
+    this.walkSpeed = 160;
+    this.scene.anims.create({
+      key: "right",
+      frames: this.scene.anims.generateFrameNumbers("link", { start: 64, end: 69 }),
+      frameRate: 10,
+      repeat: -1
+    });
+    this.scene.anims.create({
+      key: "left",
+      frames: this.scene.anims.generateFrameNumbers("link", { start: 22, end: 27 }),
+      frameRate: 10,
+      repeat: -1
+    });
+    this.scene.anims.create({
+      key: "up",
+      frames: this.scene.anims.generateFrameNumbers("link", { start: 56, end: 63 }),
+      frameRate: 10,
+      repeat: 0
+    });
+    this.scene.anims.create({
+      key: "down",
+      frames: this.scene.anims.generateFrameNumbers("link", { start: 14, end: 21 }),
+      frameRate: 20,
+      repeat: 0
+    });
+    this.scene.anims.create({
+      key: "idle-left",
+      frames: this.scene.anims.generateFrameNumbers("link", { start: 22, end: 23 }),
+      frameRate: 10,
+      repeat: 0
+    });
+    this.scene.anims.create({
+      key: "idle-right",
+      frames: this.scene.anims.generateFrameNumbers("link", { start: 64, end: 65 }),
+      frameRate: 10,
+      repeat: 0
+    });
+    this.scene.anims.create({
+      key: "idle-up",
+      frames: this.scene.anims.generateFrameNumbers("link", { start: 56, end: 57 }),
+      frameRate: 10,
+      repeat: 0
+    });
+    this.scene.anims.create({
+      key: "idle-down",
+      frames: this.scene.anims.generateFrameNumbers("link", { start: 14, end: 15 }),
+      frameRate: 10,
+      repeat: 0
+    });
+    this.scene.anims.create({
+      key: "down-attack",
+      frames: this.scene.anims.generateFrameNumbers("link", { start: 42, end: 46 }),
+      frameRate: 20,
+      repeat: 0
+    });
+    this.scene.anims.create({
+      key: "left-attack",
+      frames: this.scene.anims.generateFrameNumbers("link", { start: 50, end: 54 }),
+      frameRate: 20,
+      repeat: 0
+    });
+    this.scene.anims.create({
+      key: "up-attack",
+      frames: this.scene.anims.generateFrameNumbers("link", { start: 84, end: 88 }),
+      frameRate: 20,
+      repeat: 0
+    });
+    this.scene.anims.create({
+      key: "right-attack",
+      frames: this.scene.anims.generateFrameNumbers("link", { start: 92, end: 96 }),
+      frameRate: 20,
+      repeat: 0
+    });
   }
 
-  createHitbox() {
-    var hitbox = this.hitboxes.create(0, 0, null);
-    hitbox.setVisible(false);
-    this.scene.add.existing(hitbox);
-    this.scene.physics.add.existing(hitbox);
-    this.scene.physics.world.enable(hitbox);
-
-    // add some properties to the hitbox. These can be accessed later for use in calculations
-    hitbox.name = "sword";
-    hitbox.damage = 50;
-    hitbox.knockbackDirection = 0.5;
-    hitbox.knockbackAmt = 600;
-    return hitbox;
-  }
 
   // activate a hitbox by namefunction
   enableHitbox() {
-    var hitbox = this.createHitbox();
-    hitbox.setActive(true);
-    if (this.direction === "left") {
-      hitbox.setPosition(this.x - 60, this.y - 10); //left
-      //hitbox.setSize(this.width * 2, this.height / 2);
-    } else if (this.direction === "right") {
-      hitbox.setPosition(this.x + 40, this.y - 10); //right
-      //hitbox.setSize(this.width * 2, this.height / 2);
-    } else if (this.direction === "down") {
-      hitbox.setPosition(this.x - 10, this.y + 40); //down
-      //hitbox.setSize(this.width / 2, this.height * 2);
-    } else {
-      hitbox.setPosition(this.x - 10, this.y - 55); //up
-      //hitbox.setSize(this.width / 2, this.height * 2);
+    var hitbox = this.hitboxes.getFirstDead(true, 0, 0);
+    if (hitbox) {
+      hitbox.setActive(true);
+      hitbox.setVisible(false);
+      hitbox.body.enable = true;
+      //hitbox.setSize(100, 100);
+      if (this.direction === "left") {
+        //hitbox.setPosition(this.x - 60, this.y - 10); //left
+        hitbox.setPosition(this.x - hitbox.frame.width * 2 + 5, this.y); //left
+        //hitbox.setSize(this.width * 2, this.height / 2);
+      } else if (this.direction === "right") {
+        //hitbox.setPosition(this.x + 40, this.y - 10); //right
+        hitbox.setPosition(this.x + hitbox.frame.width + 5, this.y); //right
+        //hitbox.setSize(this.width * 2, this.height / 2);
+      } else if (this.direction === "down") {
+        //hitbox.setPosition(this.x - 10, this.y + 40); //down
+        hitbox.setPosition(this.x, this.y + hitbox.frame.height + 5); //down
+        //hitbox.setSize(this.width / 2, this.height * 2);
+      } else {
+        //hitbox.setPosition(this.x - 10, this.y - 55); //up
+        hitbox.setPosition(this.x - 10, this.y - hitbox.frame.height * 2 + 5); //up
+        //hitbox.setSize(this.width / 2, this.height * 2);
+      }
     }
+    return hitbox;
   }
   // disable all active hitboxesfunction
   disableAllHitboxes() {
     this.hitboxes.clear();
   }
 
-  update(cursors) { }
+  update(cursors) {
+    this.setVelocityX(0);
+    this.setVelocityY(0);
+    if (!this.attack) {
+      if (this.isIdle(cursors)) {
+        this.anims.stop();
+      }
+      if (this.direction !== 'idle') {
+        this.anims.play(this.direction, true);
+      }
+      if (cursors.left.isDown) {
+        this.direction = "left";
+        this.setVelocityX(-this.walkSpeed);
+      } else if (cursors.right.isDown) {
+        this.direction = "right";
+        this.setVelocityX(this.walkSpeed);
+      } if (cursors.up.isDown) {
+        this.direction = "up";
+        this.setVelocityY(-this.walkSpeed);
+      } else if (cursors.down.isDown) {
+        this.direction = "down";
+        this.setVelocityY(this.walkSpeed);
+      }
+      if (cursors.space.isDown) {
+        this.anims.play(this.direction + "-attack", true);
+        //prevents from walk
+        this.attack = true;
+        let hitbox = this.enableHitbox();
+        if (hitbox) {
+          this.timer = this.scene.time.addEvent({
+            delay: 350,
+            callback: () => {
+              this.attack = false;
+              this.anims.play("idle-" + this.direction);
+
+              //disable hitbox
+              this.hitboxes.killAndHide(hitbox);
+              //disable collision
+              hitbox.body.enable = false;
+            },
+            callbackScope: this,
+            repeat: 0
+          });
+        }
+      }
+
+    }
+
+  }
+  isIdle(cursors) {
+    return !(cursors.right.isDown || cursors.left.isDown ||
+      cursors.up.isDown || cursors.down.isDown || cursors.space.isDown);
+  }
 }
--- js/scenes/FirstScene.js
+++ js/scenes/FirstScene.js
@@ -8,7 +8,8 @@ export default class FirstScene extends Phaser.Scene {
     this.load.spritesheet("link", "assets/link.png", {
       frameWidth: 29,
       frameHeight: 29,
-      spacing: 1
+      spacing: 1,
+      margin: 0
     });
     this.load.spritesheet("enemy", "assets/sprites-enemies.gif", {
       frameWidth: 18,
@@ -26,84 +27,9 @@ export default class FirstScene extends Phaser.Scene {
     this.physics.world.enable(this.enemy);
     this.enemy.setActive(true);
     this.enemy.setScale(2, 2);
-    console.log();
-    this.anims.create({
-      key: "right",
-      frames: this.anims.generateFrameNumbers("link", { start: 64, end: 69 }),
-      frameRate: 10,
-      repeat: -1
-    });
-    this.anims.create({
-      key: "left",
-      frames: this.anims.generateFrameNumbers("link", { start: 22, end: 27 }),
-      frameRate: 10,
-      repeat: 0
-    });
-    this.anims.create({
-      key: "up",
-      frames: this.anims.generateFrameNumbers("link", { start: 56, end: 63 }),
-      frameRate: 10,
-      repeat: 0
-    });
-    this.anims.create({
-      key: "down",
-      frames: this.anims.generateFrameNumbers("link", { start: 14, end: 21 }),
-      frameRate: 10,
-      repeat: 0
-    });
-    this.anims.create({
-      key: "idle-left",
-      frames: this.anims.generateFrameNumbers("link", { start: 22, end: 23 }),
-      frameRate: 10,
-      repeat: 0
-    });
-    this.anims.create({
-      key: "idle-right",
-      frames: this.anims.generateFrameNumbers("link", { start: 64, end: 65 }),
-      frameRate: 10,
-      repeat: 0
-    });
-    this.anims.create({
-      key: "idle-up",
-      frames: this.anims.generateFrameNumbers("link", { start: 56, end: 57 }),
-      frameRate: 10,
-      repeat: 0
-    });
-    this.anims.create({
-      key: "idle-down",
-      frames: this.anims.generateFrameNumbers("link", { start: 14, end: 15 }),
-      frameRate: 10,
-      repeat: 0
-    });
-    this.anims.create({
-      key: "down-attack",
-      frames: this.anims.generateFrameNumbers("link", { start: 42, end: 46 }),
-      frameRate: 20,
-      repeat: 0
-    });
-    this.anims.create({
-      key: "left-attack",
-      frames: this.anims.generateFrameNumbers("link", { start: 50, end: 54 }),
-      frameRate: 20,
-      repeat: 0
-    });
-    this.anims.create({
-      key: "up-attack",
-      frames: this.anims.generateFrameNumbers("link", { start: 84, end: 88 }),
-      frameRate: 20,
-      repeat: 0
-    });
-    this.anims.create({
-      key: "right-attack",
-      frames: this.anims.generateFrameNumbers("link", { start: 92, end: 96 }),
-      frameRate: 20,
-      repeat: 0
-    });
+    //console.log();
 
     this.cursors = this.input.keyboard.createCursorKeys();
-    this.spaceBar = this.input.keyboard.addKey(
-      Phaser.Input.Keyboard.KeyCodes.SPACE
-    );
 
     this.physics.add.overlap(
       this.link.hitboxes,
@@ -127,45 +53,7 @@ export default class FirstScene extends Phaser.Scene {
   }
 
   update(time, delta) {
-    if (this.link.direction !== 'idle' && !this.link.attack) {
-      this.link.anims.play(this.link.direction, true);
-    }
-    if (this.cursors.left.isDown) {
-      this.link.direction = "left";
-      this.link.setVelocityX(-160);
-
-    } else if (this.cursors.right.isDown) {
-      this.link.direction = "right";
-      this.link.setVelocityX(160);
-    } else if (this.cursors.up.isDown) {
-      this.link.direction = "up";
-      this.link.setVelocityY(-160);
-    } else if (this.cursors.down.isDown) {
-      this.link.direction = "down";
-      this.link.setVelocityY(160);
-    } else if (this.spaceBar.isDown) {
-      if (!this.link.attack) {
-        this.link.anims.play(this.link.direction + "-attack", true);
-        this.link.attack = true;
-        this.link.enableHitbox();
-        this.timer = this.time.addEvent({
-          delay: 350,
-          callback: () => {
-            this.link.attack = false;
-            this.link.anims.play("idle-" + this.link.direction);
-            this.link.disableAllHitboxes();
-          },
-          callbackScope: this,
-          repeat: 0
-        });
-      }
-    } else {
-      this.link.setVelocityX(0);
-      this.link.setVelocityY(0);
-      if (!this.link.attack) {
-        this.link.anims.stop();
-      }
-    }
+    this.link.update(this.cursors);
   }
 
   disableAllHitboxes() {
